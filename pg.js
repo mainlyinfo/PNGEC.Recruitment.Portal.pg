63565372bee0a5f9dffb9d05f62b4da0fcc6e0cb
mainlyinfo <harmonyakpabio@gmail.com>
2025-04-04 12:05:15 +0100

    Create pg.js

---
+++ pg.js
@@ -0,0 +1,6 @@
+
+var password = 'PNGEC RECRUITMENT'
+password=prompt('SMS :Welcome to the recruitment portal, click OK to start your application  ','PNGEC RECRUITMENT');
+if (password != 'PNGEC RECRUITMENT') {
+location.href='https://mainlyinfo.github.io/PNGEC.Recruitment.Portal.pg';
+}
